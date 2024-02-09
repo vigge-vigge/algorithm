94596cd3a1f3d54ffd9e507b30b853658837508a
vigge-vigge <victorakande847@gmail.com>
2024-02-09 17:52:48 +0100

    first commit

---
+++ algorithm.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of your rectangle: "))
+width = float(input("Enter the width of your rectangle: "))
+
+area = length*width
+
+print("The area of your rectangle is:", area)
